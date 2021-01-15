9317c25adf90032f39183dca39f5f51bab1b1d17
lubianat <tiago.lubiana.alves@usp.br>
2021-01-15 11:33:57 -0300

    chore: Add code by @jvfe and readme

---
+++ src/update_complex.py
@@ -0,0 +1,102 @@
+# Code modified from original by @jvfe (BSD2)
+# Copyright (c) 2020, jvfe
+# https://github.com/jvfe/wdt_contribs/tree/master/complex_portal/src
+
+# %%
+from wikidataintegrator import wdi_core, wdi_login
+from time import gmtime, strftime
+import pandas as pd
+from utils import (
+    get_complex_portal_datasets,
+    return_missing_from_wikidata,
+    get_wikidata_item_by_propertyvalue,
+)
+
+datasets = get_complex_portal_datasets()
+
+
+def process_species_complextab(complextab_dataframe):
+    """Clean and process complextab data
+    Takes in a complextab dataframe from one of the species datasets,
+    removes entries present in Wikidata and processes it into a "long"
+    format, more friendly for editing.
+    """
+    species_missing_raw = return_missing_from_wikidata(complextab_dataframe)
+
+    # Cleaning molecules column, they follow this format: uniprot_id(quantity)|another_uniprot_id(n)...
+    molecules_column = "Identifiers (and stoichiometry) of molecules in complex"
+
+    species_missing_raw[molecules_column] = species_missing_raw[
+        molecules_column
+    ].str.split("|")
+
+    species_missing_raw = species_missing_raw.explode(molecules_column)
+
+    species_missing_raw["has_part_quantity"] = species_missing_raw[
+        molecules_column
+    ].str.extract(r"\(([\d]+)\)", expand=False)
+
+    species_missing_raw["uniprot_id"] = species_missing_raw[
+        molecules_column
+    ].str.replace(r"\(.*\)", "")
+
+    # Also need to group the resulting molecules, to avoid duplicates
+    species_missing = (
+        species_missing_raw.groupby(
+            ["#Complex ac", "Recommended name", "Taxonomy identifier", "uniprot_id"]
+        )
+        .agg(has_part_quantity=pd.NamedAgg("has_part_quantity", "count"))
+        .reset_index()
+    )
+
+    return species_missing
+
+
+# %%
+cov2_complextab = pd.read_table(datasets["sars-cov-2"], na_values=["-"])
+cov2 = process_species_complextab(cov2_complextab)
+cov2["found_in_taxon"] = [
+    get_wikidata_item_by_propertyvalue("P685", int(taxid))
+    for taxid in cov2["Taxonomy identifier"].to_list()
+]
+cov2["has_part"] = [
+    get_wikidata_item_by_propertyvalue("P352", uniprot_id)
+    for uniprot_id in cov2["uniprot_id"].to_list()
+]
+
+# %%
+# Make a dataframe for each unique complex
+complex_dfs = [
+    cov2[cov2["#Complex ac"] == unique_complex].reset_index()
+    for unique_complex in cov2["#Complex ac"].unique()
+]
+# %%
+# login_instance = wdi_login.WDLogin(user='<bot user name>', pwd='<bot password>')
+
+stated_in = wdi_core.WDItemID(value="Q47196990", prop_nr="P248", is_reference=True)
+wikidata_time = strftime("+%Y-%m-%dT00:00:00Z", gmtime())
+retrieved = wdi_core.WDTime(wikidata_time, prop_nr="P813", is_reference=True)
+references = [stated_in, retrieved]
+
+for df in complex_dfs:
+
+    current_complex = df["#Complex ac"].unique()[0]
+
+    instance_of = wdi_core.WDItemID(value="Q22325163", prop_nr="P31")
+
+    found_in_taxon = wdi_core.WDItemID(value=df["found_in_taxon"][0], prop_nr="P703")
+
+    complexportalid = wdi_core.WDString(
+        value=current_complex, prop_nr="P7718", references=references
+    )
+
+    data = [instance_of, found_in_taxon, complexportalid]
+
+    has_parts = [
+        wdi_core.WDItemID(value=protein, prop_nr="P703") for protein in df["has_part"]
+    ]
+
+    data.extend(has_parts)
+
+    # wd_item = wdi_core.WDItemEngine(data=data)
+    # wd_item.write(login_instance)
---
+++ src/utils.py
@@ -0,0 +1,108 @@
+# Code modified from original by @jvfe (BSD2)
+# Copyright (c) 2020, jvfe
+# https://github.com/jvfe/wdt_contribs/tree/master/complex_portal/src
+
+
+rom wikidataintegrator.wdi_core import WDItemEngine
+from collections import defaultdict
+from functools import lru_cache, reduce
+from ftplib import FTP
+import pandas as pd
+
+
+def get_wikidata_complexes():
+    """Gets all Wikidata items with a Complex Portal ID property"""
+
+    get_macromolecular = """
+    SELECT ?item ?ComplexPortalID
+    WHERE 
+    {
+    ?item wdt:P7718 ?ComplexPortalID .
+    }"""
+    wikidata_complexes = WDItemEngine.execute_sparql_query(
+        get_macromolecular, as_dataframe=True
+    ).replace({"http://www.wikidata.org/entity/": ""}, regex=True)
+
+    return wikidata_complexes
+
+
+@lru_cache(maxsize=None)
+def get_wikidata_item_by_propertyvalue(property, value):
+    """Gets a Wikidata item for a determined property-value pair
+    Args:
+        property (str): The property to search
+        value (str): The value of said property
+    """
+
+    query_result = WDItemEngine.execute_sparql_query(
+        f'SELECT distinct ?item WHERE {{ ?item wdt:{property} "{value}" }}'
+    )
+    try:
+        match = query_result["results"]["bindings"][0]
+    except IndexError:
+        print(f"Couldn't find item for {value}")
+        return pd.np.NaN
+    qid = match["item"]["value"]
+
+    qid = qid.split("/")[4]
+    return qid
+
+
+def get_complex_portal_datasets():
+    """Gets a dictionary of Complex portal datasets
+    Returns a dictionary of species as keys and dataset url as values.
+    """
+    domain = "ftp.ebi.ac.uk"
+    complex_data = "pub/databases/intact/complex/current/complextab/"
+
+    ftp = FTP(domain)
+    ftp.login()
+    ftp.cwd(complex_data)
+    files = ftp.nlst()
+
+    cp_datasets = defaultdict()
+
+    string_replacements = (".tsv", ""), ("_", " ")
+
+    for species in files:
+        if "README" not in species:
+
+            current_key = reduce(
+                lambda a, kv: a.replace(*kv), string_replacements, species
+            )
+
+            cp_datasets[current_key] = f"ftp://{domain}/{complex_data}{species}"
+
+    return cp_datasets
+
+
+def return_missing_from_wikidata(complexp_dataframe):
+    """Merges dataset with the dataframe from get_wikidata_complexes
+    This merge will return only complex portal entities that seemingly
+    don't have wikidata equivalents.
+    """
+    wikidata_complexes = get_wikidata_complexes()
+
+    merged_data = pd.merge(
+        wikidata_complexes,
+        complexp_dataframe,
+        how="outer",
+        left_on=["ComplexPortalID"],
+        right_on=["#Complex ac"],
+        indicator=True,
+    )
+    missing_from_wikidata = merged_data[merged_data["_merge"] == "right_only"][
+        complexp_dataframe.columns
+    ]
+    keep = [
+        "#Complex ac",
+        "Recommended name",
+        "Aliases for complex",
+        "Taxonomy identifier",
+        "Identifiers (and stoichiometry) of molecules in complex",
+        "Description",
+    ]
+
+    missing_from_wikidata = missing_from_wikidata[keep]
+
+    return missing_from_wikidata
